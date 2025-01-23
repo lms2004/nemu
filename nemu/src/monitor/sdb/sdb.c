8f9c554740b302a3e87af4d9b637bb24c8fa2184
tracer-ics2024 <tracer@njuics.org>
2025-01-23 14:41:02 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-1VEP92H 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:41:00 up 17:25,  1 user,  load average: 0.80, 0.58, 0.43

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -52,6 +52,14 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char* args){
+  char *arg = strtok(NULL, " ");
+  int N = atoi(arg);
+  cpu_exec(N);
+  return 0;
+}
+
+
 static int cmd_help(char *args);
 
 static struct {
@@ -62,8 +70,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
-  /* TODO: Add more commands */
+  { "si","step execute",cmd_si},
 
 };
 
