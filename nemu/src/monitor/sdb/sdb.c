c845a169364f4bc435b01895209a628f62528695
tracer-ics2024 <tracer@njuics.org>
2025-02-03 11:46:23 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-1VEP92H 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:46:18 up  2:16,  1 user,  load average: 0.89, 0.48, 0.38

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -189,7 +189,7 @@ static int cmd_test(char* path){
   char* args = calloc(2048, sizeof(char));
   char* R_expr_value = calloc(32, sizeof(char)); 
 
-  while(fscanf(fp, "%s %[^\n]", R_expr_value, args) != EOF){
+  while(fscanf(fp, "%s %[^\n]%*c", R_expr_value, args) != EOF){
     Log(" Test case %d: %s", case_i, args);
 
     char* error = calloc(128, sizeof(char));
