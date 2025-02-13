efb4f45065552a4b0322500e5f943c2eedecc901
tracer-ics2024 <tracer@njuics.org>
2025-02-14 00:07:00 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-1VEP92H 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:06:58 up  1:30,  1 user,  load average: 0.60, 0.47, 0.52

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -367,15 +367,9 @@ word_t expr(char *e, char* error) {
 }
 
 
-int check_parentheses(int* l, int* r){
-  if(tokens[*l].type == TK_NOTYPE){
-    (*l)++;
-  }
-  if(tokens[*r].type == TK_NOTYPE){
-    (*r)--;
-  }
+int check_parentheses(int l, int r){
 
-  if(tokens[*l].type != TK_LQUOTE || tokens[*r].type != TK_RQUOTE){
+  if(tokens[l].type != TK_LQUOTE || tokens[r].type != TK_RQUOTE){
     return 0;
   }
 
@@ -396,11 +390,9 @@ word_t eval(char* error, int l, int r) {
     }
     return num;
   }
-  else if(check_parentheses(&l , &r)){
+  else if(check_parentheses(l , r)){
     return eval(error, l + 1, r - 1);
   }
-  // not parentheses
-  // 
   else{
     if(tokens[l].type == TK_NOTYPE){
       return eval(error, l + 1, r);
