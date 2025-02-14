fe07c4ed2d10783d6b7f240b020e028ab936ae5b
tracer-ics2024 <tracer@njuics.org>
2025-02-14 16:21:21 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-1VEP92H 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:21:17 up  9:16,  1 user,  load average: 2.35, 1.80, 1.52

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -451,7 +451,7 @@ word_t eval(char* error, int l, int r) {
         continue;
       }
 
-      int IsOp = (op == TK_PLUS || op == TK_MUL || op == TK_DIV || op == TK_SUB || TK_AND || TK_NEQ || TK_EQ);
+      int IsOp = (op == TK_PLUS || op == TK_MUL || op == TK_DIV || op == TK_SUB || op == TK_AND || op == TK_NEQ || op == TK_EQ);
 
       if(!match && IsOp){
         int val1 = eval(error, l, i - 1);
