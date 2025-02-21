7958402f7e7ebb1244a6d56f42431c153ff36fe9
tracer-ics2024 <tracer@njuics.org>
2025-02-21 13:56:27 +0800

    >  compile NEMU Linux DESKTOP-1VEP92H 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:56:24 up  2:39,  1 user,  load average: 0.95, 0.64, 0.64

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -42,6 +42,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 
 #ifdef CONFIG_WATCHPOINT
   if(scan_wp()){  nemu_state.state = NEMU_STOP; }
+  Log("nemu: %s at pc = " FMT_WORD, ANSI_FMT("STOP", ANSI_FG_RED), cpu.pc);
 #endif
 }
 
