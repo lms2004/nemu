a55cbb4020e7f6c7f75a971637d3c426b212e56c
tracer-ics2024 <tracer@njuics.org>
2025-02-21 11:34:47 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-1VEP92H 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:34:44 up 17 min,  1 user,  load average: 0.92, 0.51, 0.41

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -40,12 +40,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
-  #ifdef CONFIG_ITRACE_COND
-  if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
+#ifdef CONFIG_WATCHPOINT
+  if(scan_wp()){  nemu_state.state = NEMU_STOP; }
 #endif
-  if(scan_wp()){
-    nemu_state.state = NEMU_STOP;
-  }
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
