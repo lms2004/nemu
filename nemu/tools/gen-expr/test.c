435f98c397e6d7c7c6c442406f76e4f7d5f096b7
tracer-ics2024 <tracer@njuics.org>
2025-02-23 18:56:14 +0800

    >  compile NEMU Linux DESKTOP-1VEP92H 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:56:10 up  4:54,  1 user,  load average: 0.74, 0.44, 0.34

--- nemu/tools/gen-expr/test.c
+++ nemu/tools/gen-expr/test.c
@@ -1,7 +1,17 @@
-#define max(a, b) ((a) > (b) ? (a) : (b))
-int main(){
-  int a = 1;
-  int b = 2;
-  int c = max(a, b);
-  return 0;
+#include "stdio.h"
+int main(int argc, char** argv)
+{
+    int a = 0xfffffffe; // -2
+    int b = a >> 1;               //signed,   arithmetic shift right
+    int c = (unsigned int)a >> 1; //unsigned, logic shift right
+    printf(" b = 0x%x (%d) \n", b,b); // b = a /2 = -1
+    printf(" c = 0x%x (%d) \n", c,c);
+ 
+    unsigned int d = 0xfffffffe; // 
+    unsigned int e = d >> 1;      //unsigned, logic shift right
+    unsigned int f = (int)d >> 1; //signed,   arithmetic shift right
+    printf(" e = 0x%x (%d) \n", e,e); 
+    printf(" f = 0x%x (%d) \n", f,f);// f = d /2 = -1
+ 
+    return 0;
 }
