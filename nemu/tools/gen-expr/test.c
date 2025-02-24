d5463faadd8ef7eb252985940ca8d806d56ab42b
tracer-ics2024 <tracer@njuics.org>
2025-02-24 09:56:28 +0800

    >  compile NEMU Linux DESKTOP-1VEP92H 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:56:25 up  1:11,  1 user,  load average: 3.31, 1.43, 0.82

--- nemu/tools/gen-expr/test.c
+++ nemu/tools/gen-expr/test.c
@@ -1,17 +1,25 @@
 #include "stdio.h"
 int main(int argc, char** argv)
 {
-    int a = 0xfffffffe; // -2
-    int b = a >> 1;               //signed,   arithmetic shift right
-    int c = (unsigned int)a >> 1; //unsigned, logic shift right
-    printf(" b = 0x%x (%d) \n", b,b); // b = a /2 = -1
-    printf(" c = 0x%x (%d) \n", c,c);
+    unsigned int ca= 1<<31;
  
-    unsigned int d = 0xfffffffe; // 
-    unsigned int e = d >> 1;      //unsigned, logic shift right
-    unsigned int f = (int)d >> 1; //signed,   arithmetic shift right
-    printf(" e = 0x%x (%d) \n", e,e); 
-    printf(" f = 0x%x (%d) \n", f,f);// f = d /2 = -1
+    unsigned  char  ucb=128;
+     
+    unsigned  short   usc=0;
+     
+    usc=ca + ucb; 
+     
+        printf("%x\n",usc);
+     
+    usc=ca +(unsigned  short)ucb; 
+        printf("%x\n",(unsigned  short)ucb);
+        printf("%x\n",usc);
+     
+    usc=(unsigned char)ca + ucb;
+     
+        printf("%x\n",usc);
+     
+    usc=ca+(char)ucb;
  
     return 0;
 }
